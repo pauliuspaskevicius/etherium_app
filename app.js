513827b43d91ea1eac2b5c2f6f879666f0b28744
Paulius Paškevičius <paulius.paskevicius@gmail.com>
2016-08-10 13:02:58 -0300

    Added: ajax

--- app.js
+++ app.js
@@ -1,33 +1,26 @@
-var items  = [
-  { 'id': 1, 'name': 'Rob', 'balance': 1001.4943 },
-  { 'id': 2, 'name': 'Felipe', 'balance': 100.01 },
-  { 'id': 3, 'name': 'Graziano', 'balance': 99.99 },
-  { 'id': 4, 'name': 'David', 'balance': 59.334 },
-  { 'id': 5, 'name': 'Paulius', 'balance': 0 }
-];
+var items  = [];
 
 $(document).ready(function(){
-  renderList (items);
+  $.ajax('data.json').done(function(response){
+    console.log(response);
+    //renderList (items);
+  });
 });
 
 function renderList (list) {
   $('#app-container').html('');
   for (var i=0; i<list.length; i++){
-
     var item = $('<div><span class="name"></span><button>x</button> <span class="balance"></span></div>');
-
     var name = $(item).find('.name').html(list[i].name);
     var balance = $(item).find('.balance').html(list[i].balance.toFixed(3)).css('float', 'right').css('padding-right', '5px');
     var button = $(item).find('button').attr('data-id', list[i].id).css('float', 'right');
     button.on('click', deleteItem);
-
-
-
     $('#app-container').append(item);
   }
 }
 
 function deleteItem () {
-  console.log($(this).attr('data-id'));
+  items[0] = null;
   console.log($(this).parent().remove());
+  console.log(items);
 }
